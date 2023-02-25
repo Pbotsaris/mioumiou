0d98586e8b70ea97c85bb4440ee6219cf6119674
Pbotsaris <pedro@antfood.com>
2023-02-25 18:30:25 +0100

    added gameObjectTosystem

--- src/ecs/world_manager.cpp
+++ src/ecs/world_manager.cpp
@@ -36,15 +36,15 @@ auto WorldManager::createGameObject() -> GameObject {
 void WorldManager::update() {}
 
 /**
- * Creates a component type T and add to a component pool Pool<T>.
+ * Creates a component<T> and add to a component pool Pool<T>.
  * The new component is then registered a GameObject in
  * m_gameObjectcomponentSignatures accordingly. This method will grow the
  * m_componentPools vectors as well contained Pool<T> as necessary.
  *
  * @tparam T: A Component type
- * @tparam TARGS: The Component constructor variadic arguments types
- * @param gameObject: The game object
- * @param args: the arguments to create a new component type T
+ * @tparam TARGS: Component<T> constructor parameter types
+ * @param gameObject: GameObject to add a component to
+ * @param args: Component<T> constructor parameters
  * @return void
  */
 
@@ -78,17 +78,17 @@ void WorldManager::addComponent(GameObject gameObject, TARGS &&...args) {
 }
 
 /**
- * Removes a component type T from a GameObject
+ * Removes a Component<T> from a GameObject
  * m_gameObjectcomponentSignatures bitset for the GameObject in question.
  * Note the the component pool remains unmodified.
  *
  * @tparam T: A Component type
- * @param gameObject: The game object
+ * @param gameObject: A GameObject to remove a Component<T> from
  * @return void
  */
 
 template <typename T>
-void WorldManager::RemoveComponent(GameObject gameObject) {
+void WorldManager::removeComponent(GameObject gameObject) {
   const uint32_t componentId = Component<T>::id();
 
   // set to 0
@@ -96,10 +96,10 @@ void WorldManager::RemoveComponent(GameObject gameObject) {
 }
 
 /**
- * Checks if a GameObject has either Component type <T> or not.
+ * Checks if a GameObject has either Component<T> or not.
  *
  * @tparam T: A Component type
- * @param gameObject: The game object
+ * @param gameObject: A GameObject to check the Component<T> existance
  * @return boolean
  */
 
@@ -109,6 +109,14 @@ auto WorldManager::hasComponent(GameObject gameObject) const -> bool {
   return m_gameObjectcomponentSignatures.at(gameObject.id()).test(componentId);
 }
 
+/**
+ * Gets a Component<T> from a GameObject
+ *
+ * @tparam T: A Component type
+ * @param gameObject:  GameObject to get a Component<T> from
+ * @return Component<T> reference
+ */
+
 template <typename T>
 auto WorldManager::getComponent(GameObject gameObject) const -> T & {
   const uint32_t componentId = Component<T>::id();
@@ -116,6 +124,90 @@ auto WorldManager::getComponent(GameObject gameObject) const -> T & {
   Pool<T> pool = m_componentPools.at(componentId);
 
   if (hasComponent<T>(gameObject)) {
-    return pool.at(gameObject.id());
+    return *(std::static_pointer_cast<T>(pool.at(gameObject.id())));
+  }
+}
+
+/**
+ * Creates a new System<T> in the world.
+ *
+ * @tparam T: A System type
+ * @tparam TARGS: System<T> constructor argument types.
+ * @param ...args: System<T> constructor arguments.
+ */
+
+template <typename T, typename... TARGS>
+void WorldManager::createSystem(TARGS &&...args) {
+
+  T sys = std::move(new T(std::forward<TARGS>(args)...));
+  // TODO: check if std::type_index(*sys) would also work to simplify the syntax
+  m_systems.insert(std::make_pair(std::type_index(typeid(T)), sys));
+}
+
+/**
+ * Removes a System type <T> from the world.
+ *
+ * @tparam T: A System type
+ */
+
+template <typename T> void WorldManager::removeSystem() {
+  // TODO: delete System. future smart pointer?
+  m_systems.find(std::type_index(typeid(T)));
+}
+
+/**
+ * Checks wether a System<T> exists in the world or not.
+ *
+ * @tparam T: A System type
+ * @return: boolean
+ */
+
+template <typename T> auto WorldManager::hasSystem() const -> bool {
+  return m_systems.find(std::type_index(typeid(T))) != m_systems.end();
+}
+
+/**
+ * Gets a System<T> from the word.
+ *
+ * @tparam T: A System type
+ * @return: A reference to the System<T>.
+ */
+
+template <typename T> auto WorldManager::getSystem() const -> T & {
+  // 1. find the map iterator pointer by the key.
+  // 2. get value from pair(iter pair)  with ->second
+  // 3. cast to T* and dereference for return.
+  return *(std::static_pointer_cast<T>(
+      m_systems.find(std::type_index(typeid(T)))->second));
+}
+
+/* PRIVATE */
+
+/**
+ * Adds a GameObject System where the GameObject Signatures satifies the Components requirements
+ * in System::m_componentSignature.
+ * a GameObject signature must contain all system component requirements in its signature.
+ * @param gameObject: A GameObject
+ */
+
+
+void WorldManager::gameObjectToSystems(GameObject gameObject) {
+  const Signature &gameObjSig = m_gameObjectcomponentSignatures.at(gameObject.id());
+
+  for (auto &keypair : m_systems) {
+    System *sys = keypair.second;
+
+/* 
+ *  NOTE: 
+ *  The bitwise &(AND) between the GameObject Signature and System Signature must be equals to System Signature 
+ *  otherwise the GameObject either don't have all components required by the System
+ *  or has components that the System does not support.
+ */
+   
+    bool satisfied = (sys->componentSignature() & gameObjSig)  == sys->componentSignature();
+
+    if (satisfied) {
+      sys->addGameObject(gameObject);
+    }
   }
 }
--- src/ecs/world_manager.hpp
+++ src/ecs/world_manager.hpp
@@ -21,20 +21,25 @@ class WorldManager {
     /* game objects are only actually added to game world when update is called */
     void update();
 
-    /* INFO: GameObject Management */
+    /* GameObject Management */
     auto createGameObject() -> GameObject;
     void removeGameObject(GameObject gameObject);
     /* */
 
     
-    /* INFO: Component Management */
+    /* Component Management */
     template<typename T, typename ...TARGS> void addComponent(GameObject gameObject, TARGS &&...args);
-    template<typename T> void RemoveComponent(GameObject gameObject);
+    template<typename T> void removeComponent(GameObject gameObject);
     template<typename T> auto getComponent(GameObject gameObject) const -> T&;
     template<typename T> auto hasComponent(GameObject gameObject) const -> bool;
     /* */
 
-    void addSystem(System sys);
+    /* System Management */
+    template<typename T, typename ...TARGS> void createSystem(TARGS &&...args);
+    template<typename T> void removeSystem();
+    template<typename T> auto hasSystem() const -> bool;
+    template<typename T> auto getSystem() const -> T&;
+    /*  */
 
   private:
     std::uint32_t m_gameObjectCount = 0;
@@ -42,20 +47,25 @@ class WorldManager {
     /* Components pooled by type 
      * [vector index = Components<T>::id()], [pool index = GameObject::id()]
      * */
+
     std::vector<PoolInterface*> m_componentPools;
 
     /* Signature will allow manager to to know what components are set for a given GameObject 
      * vector index = GameObject::id()
      * m_gameObjectcomponentSignatures[GameObject::id()] = componentIds;
      * */
+
     std::vector<Signature> m_gameObjectcomponentSignatures;
 
     /* map of system where key = System type using std::type_index */
-    std::unordered_map<std::type_index, System*> systems;
-
+    std::unordered_map<std::type_index, System*> m_systems;
     /* sets as queues insertion and removal of GameObjects */
     std::set<GameObject> m_gameObjectAddQueue;
+
     std::set<GameObject> m_gameObjectRemoveQueue;
+
+
+    void gameObjectToSystems(GameObject gameObject);
 };
 
 #endif
